5e52e0015ce5a35c0cc4f35a4893cfd90b2f929d
AtesiT <atesit@mail.ru>
2026-03-11 16:26:18 +0300

    ADD: UITextViewRepresentation (where we can add a label)

--- OneSliderOnUIKitInSwiftUI/ContentView.swift
+++ OneSliderOnUIKitInSwiftUI/ContentView.swift
@@ -4,6 +4,13 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
+            HStack {
+                UITextViewRepresentation(theWord: "0", theColor: .black)
+                    .fixedSize()
+                UISliderViewRepresentation(sliderValue: .constant(50.0))
+                UITextViewRepresentation(theWord: "100", theColor: .black)
+                    .fixedSize()
+            }
         }
         .padding()
     }
