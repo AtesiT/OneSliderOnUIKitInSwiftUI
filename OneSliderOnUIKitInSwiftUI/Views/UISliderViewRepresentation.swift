ddf12bc6999c76725a87e1cb837479e5e7f66752
AtesiT <atesit@mail.ru>
2026-03-11 15:06:46 +0300

    ADD: Slider (that was init by UIKit)

---
+++ OneSliderOnUIKitInSwiftUI/Views/UISliderViewRepresentation.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct UISliderViewRepresentation: UIViewRepresentable {
+    @Binding var sliderValue: Float
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        sliderValue = uiView.value
+    }
+}
+
+#Preview {
+    UISliderViewRepresentation(sliderValue: .constant(50))
+}
