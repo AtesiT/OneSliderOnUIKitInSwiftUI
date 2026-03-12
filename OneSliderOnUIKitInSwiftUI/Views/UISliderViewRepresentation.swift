c0bbad0c78492cb42c22d487374fbed4d1444e0d
AtesiT <atesit@mail.ru>
2026-03-12 23:11:52 +0300

    ADD: addTarget and was updated updateUIView(to change color of slider (not the button for now))

--- OneSliderOnUIKitInSwiftUI/Views/UISliderViewRepresentation.swift
+++ OneSliderOnUIKitInSwiftUI/Views/UISliderViewRepresentation.swift
@@ -2,17 +2,41 @@ import SwiftUI
 
 struct UISliderViewRepresentation: UIViewRepresentable {
     @Binding var sliderValue: Float
+    var targetValue: Float = 100
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider()
         slider.minimumValue = 0
         slider.maximumValue = 100
         slider.translatesAutoresizingMaskIntoConstraints = false
+        
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.sliderValueChanged(_:)),
+            for: .valueChanged
+        )
+        
         return slider
     }
     
     func updateUIView(_ uiView: UISlider, context: Context) {
-        sliderValue = uiView.value
+        uiView.value = sliderValue
+        
+        // Используем abs
+        let range = abs(uiView.value - targetValue)
+        let colorSlider = CGFloat(1.0 - (range / uiView.maximumValue))
+        
+        //  Установка цвета
+        uiView.minimumTrackTintColor = UIColor(
+            red: colorSlider,
+            green: 1.0 - colorSlider,
+            blue: 1.0 - colorSlider,
+            alpha: 1.0
+        )
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(sliderValue: $sliderValue)
     }
     
     class Coordinator: NSObject {
