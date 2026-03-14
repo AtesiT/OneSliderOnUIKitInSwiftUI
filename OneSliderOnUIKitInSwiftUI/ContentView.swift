a5ea9d32c8c5df56c78e66d48b42d76f468d74fd
AtesiT <atesit@mail.ru>
2026-03-14 15:42:43 +0300

    ADD: showAlert, ability to init random number to find its

--- OneSliderOnUIKitInSwiftUI/ContentView.swift
+++ OneSliderOnUIKitInSwiftUI/ContentView.swift
@@ -1,20 +1,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    //  Число, которое необходимо найти; Первоначальное значение слайдера; Булевое значение alert (показывается или скрыт)
+    @State private var target = Int.random(in: 0...100)
+    @State private var currentValue: Float = 50.0
+    @State private var showAlert = false
+    
     var body: some View {
         VStack {
-            UITextViewRepresentation(theWord: "Подвиньте слайдер, как можно ближе к: ", theColor: .black)
+            UITextViewRepresentation(theWord: "Подвиньте слайдер, как можно ближе к: \(target)", theColor: .black)
                 .fixedSize()
                 .padding()
             HStack {
                 UITextViewRepresentation(theWord: "0", theColor: .black)
                     .fixedSize()
-                UISliderViewRepresentation(sliderValue: .constant(50.0))
+                UISliderViewRepresentation(sliderValue: $currentValue)
                 UITextViewRepresentation(theWord: "100", theColor: .black)
                     .fixedSize()
             }
+            //  Добавляем alert
+            .alert("Результат", isPresented: $showAlert) {
+                Button("OK") {}
+            } message: {
+                let score = toDisplayResult()
+                Text("Вы набрали \(score) баллов из 100!")
+            }
+            
             UIButtonViewRepresentation(theColor: .blue, textButton: "Проверь меня!") {
-                print("Check me!")
+                showAlert = true
             }
                 .fixedSize()
                 .padding()
@@ -24,6 +37,12 @@ struct ContentView: View {
                 .fixedSize()
         }
         .padding()
+    }
+    
+    //  Функция для подсчёта очков с округлением числа в меньшую сторону
+    func toDisplayResult() -> Int {
+        let result = abs(target - lround(Double(currentValue)))
+        return 100 - result
     }
 }
 
